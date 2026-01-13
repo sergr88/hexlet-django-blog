7e1368bff8a46ca0dacbf1fbbc169cdba115dd7c
Сергей Князьков <knyazkovsv@rushydro.ru>
2026-01-14 01:02:05 +0400

    Добавлен шаблон страницы index приложения article

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('article')
+    return render(
+        request,
+        "article_index.html",
+        context={
+            "application_name": "Статьи",
+        },
+    )
